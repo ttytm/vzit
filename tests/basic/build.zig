b0aa3b524e7d997591930071442b4b1502febda8
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-06-06 20:59:17 +0200

    test: update to include and test new `-force` flag

--- tests/basic/build.zig
+++ tests/basic/build.zig
@@ -4,7 +4,7 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
+	// Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
